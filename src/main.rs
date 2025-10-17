7b2dd87981348339d5cf093684d8892d1dbffc90
Ochir Battulga <ochir.b@denode.io>
2025-10-17 13:39:34 +0800

    improvement

--- src/main.rs
+++ src/main.rs
@@ -1,93 +1,222 @@
 use std::io;
 
-fn main() {
-    let mut board: Vec<Vec<char>> = vec![vec![' '; 3]; 3];
-    let mut current_player: char = 'O';
+struct Player {
+    name: char,
+    score: usize,
+}
 
-    loop {
-        println!("======================================");
-        draw_board(&board);
-        play_turn(&mut board, current_player);
-        if check_over(&mut board, current_player) {
-            break;
+struct TicTacToeApp {
+    board: [[char; 3]; 3],
+    current_player_index: usize,
+    game_over: bool,
+    winner: Option<char>,
+    message: String,
+    quit: bool,
+    player_one: Player,
+    player_two: Player,
+}
+
+impl Default for TicTacToeApp {
+    fn default() -> Self {
+        let player_one = Player {
+            name: 'X',
+            score: 0,
+        };
+        let player_two = Player {
+            name: 'O',
+            score: 0,
+        };
+        Self {
+            board: [[' '; 3]; 3],
+            player_one,
+            player_two,
+            current_player_index: 0,
+            game_over: false,
+            winner: None,
+            message: String::from("Player X's turn"),
+            quit: false,
         }
-        current_player = if current_player == 'X' { 'O' } else { 'X' };
     }
 }
 
-fn check_over(board: &mut Vec<Vec<char>>, player: char) -> bool {
-    let win = (0..3).any(|i| {
-        (board[i][0] == player && board[i][1] == player && board[i][2] == player)
-            || (board[0][i] == player && board[1][i] == player && board[2][i] == player)
-            || (board[0][0] == player && board[1][1] == player && board[2][2] == player
-                || board[0][2] == player && board[1][1] == player && board[2][0] == player)
-    });
-    if win {
-        println!("Player {player} won!");
-        return true;
-    }
-    if board.iter().all(|r| r.iter().all(|&c| c != ' ')) {
-        println!("It's a draw!");
-        return true;
-    }
-    false
-}
-
-fn play_turn(board: &mut Vec<Vec<char>>, player: char) {
-    loop {
-        let pos = take_player_input(player);
-        let row = (pos - 1) / 3;
-        let col = (pos - 1) % 3;
-
-        if board[row as usize][col as usize] == ' ' {
-            board[row as usize][col as usize] = player;
-            break;
+impl TicTacToeApp {
+    fn get_current_player(&self) -> &Player {
+        if self.current_player_index == 0 {
+            &self.player_one
         } else {
-            println!("Spot is taken Choose another");
-            continue;
+            &self.player_two
         }
     }
-}
 
-fn take_player_input(current_player: char) -> u32 {
-    loop {
-        let mut input = String::new();
-        println!("Player {current_player} enter your position: ");
+    fn get_current_player_mut(&mut self) -> &mut Player {
+        if self.current_player_index == 0 {
+            &mut self.player_one
+        } else {
+            &mut self.player_two
+        }
+    }
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("Failed to read line");
+    fn switch_player(&mut self) {
+        self.current_player_index = if self.current_player_index == 0 { 1 } else { 0 }
+    }
 
-        match input.trim().parse::<u32>() {
-            Ok(num) if num >= 1 && num <= 9 => return num,
-            _ => {
-                println!("Invalid input. please enter number between 1 and 9");
-                continue;
+    fn reset(&mut self) {
+        self.board = [[' '; 3]; 3];
+        self.current_player_index = 0;
+        self.game_over = false;
+        self.winner = None;
+        self.message = String::from("Player X's turn");
+    }
+
+    fn make_move(&mut self, row: usize, col: usize) {
+        if self.game_over || self.board[row][col] != ' ' {
+            return;
+        }
+
+        let current_name = self.get_current_player().name;
+        self.board[row][col] = current_name;
+
+        if self.check_win(current_name) {
+            self.game_over = true;
+            self.winner = Some(self.get_current_player().name);
+            self.get_current_player_mut().score += 1;
+            self.message = format!("Player {} wins!", current_name);
+            self.ask_continue();
+        } else if self.is_board_full() {
+            self.game_over = true;
+            self.winner = None;
+            self.message = format!("It's a Draw!");
+            self.ask_continue();
+        } else {
+            self.switch_player();
+            let newgame = self.get_current_player().name;
+            self.message = format!("Player {}'s turn", newgame)
+        }
+    }
+
+    fn ask_continue(&mut self) {
+        println!("Play again? [y/n]");
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            let trimmed = input.trim().to_lowercase();
+
+            if let Some(c) = trimmed.chars().next() {
+                match c {
+                    'n' => {
+                        self.quit = true;
+                        self.display_scores();
+                        break;
+                    }
+                    'y' => {
+                        self.reset();
+                        break;
+                    }
+                    _ => println!("Please enter 'y' or 'n'"),
+                }
+            } else {
+                println!("Please enter 'y' or 'n'")
             }
         }
     }
-}
 
-fn draw_board(board: &Vec<Vec<char>>) {
-    println!();
-    for (i, row) in board.iter().enumerate() {
-        let display_row: Vec<String> = row
-            .iter()
-            .enumerate()
-            .map(|(j, &cell)| {
-                if cell == ' ' {
-                    (i * 3 + j + 1).to_string()
-                } else {
-                    cell.to_string()
+    fn check_win(&self, player: char) -> bool {
+        for row in 0..3 {
+            if self.board[row][0] == player
+                && self.board[row][1] == player
+                && self.board[row][2] == player
+            {
+                return true;
+            }
+        }
+
+        for col in 0..3 {
+            if self.board[0][col] == player
+                && self.board[1][col] == player
+                && self.board[2][col] == player
+            {
+                return true;
+            }
+        }
+
+        if (self.board[0][0] == player && self.board[1][1] == player && self.board[2][2] == player)
+            || (self.board[0][2] == player
+                && self.board[1][1] == player
+                && self.board[2][0] == player)
+        {
+            return true;
+        }
+
+        false
+    }
+
+    fn is_board_full(&self) -> bool {
+        self.board.iter().all(|row| row.iter().all(|&c| c != ' '))
+    }
+
+    fn take_input(&self) -> (usize, usize) {
+        loop {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).unwrap();
+            match input.trim().parse::<i32>() {
+                Ok(num) if num >= 1 && num <= 9 => {
+                    return (((num - 1) / 3) as usize, ((num - 1) % 3) as usize);
                 }
-            })
-            .collect();
-        println!(
-            " {} | {} | {}",
-            display_row[0], display_row[1], display_row[2]
-        );
-        if i < 2 {
-            println!("---------")
+                _ => println!("Please enter number between 1 and 9"),
+            }
         }
     }
+
+    fn draw_board(&self) {
+        println!();
+        for (i, row) in self.board.iter().enumerate() {
+            let display_row: Vec<String> = row
+                .iter()
+                .enumerate()
+                .map(|(j, &cell)| {
+                    if cell == ' ' {
+                        (i * 3 + j + 1).to_string()
+                    } else {
+                        cell.to_string()
+                    }
+                })
+                .collect();
+            println!(
+                " {} | {} | {}",
+                display_row[0], display_row[1], display_row[2]
+            );
+            if i < 2 {
+                println!("---------")
+            }
+        }
+    }
+
+    fn display_scores(&self) {
+        println!("╔════════════════════════════╗");
+        println!("║         SCORES             ║");
+        println!("╠════════════════════════════╣");
+        println!(
+            "║ Player {} : {:2}             ║",
+            self.player_one.name, self.player_one.score
+        );
+        println!(
+            "║ Player {} : {:2}             ║",
+            self.player_two.name, self.player_two.score
+        );
+        println!("╚════════════════════════════╝");
+    }
+    fn play(&mut self) {
+        println!("{}", self.message);
+        while !self.quit {
+            self.draw_board();
+            let (row, col) = self.take_input();
+            self.make_move(row, col);
+            println!("{}", self.message)
+        }
+    }
+}
+
+fn main() {
+    let mut game = TicTacToeApp::default();
+    game.play();
 }
